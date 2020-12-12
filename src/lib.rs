7aa107c65307e5fe580dc5459705e6d7c501649b
Artem Vorotnikov <artem@vorotnikov.me>
2020-12-12 17:47:13 +0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#[doc(hidden)]
+pub extern crate bytes;
+
+#[doc(hidden)]
+pub extern crate hex_literal;
+
+#[macro_export]
+macro_rules! bytes {
+    ( $x:expr ) => {
+        $crate::bytes::Bytes::from_static(&$crate::hex_literal::hex!($x))
+    };
+}
