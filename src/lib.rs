9e789cfeb7b52f19bd765d8550450afbb23bdcef
Artem Vorotnikov <artem@vorotnikov.me>
2021-03-04 15:10:22 +0300

    lifetimed-bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #[doc(hidden)]
-pub extern crate bytes;
+pub extern crate lifetimed_bytes;
 
 #[doc(hidden)]
 pub extern crate hex_literal;
@@ -7,6 +7,19 @@ pub extern crate hex_literal;
 #[macro_export]
 macro_rules! bytes {
     ( $x:expr ) => {
-        $crate::bytes::Bytes::from_static(&$crate::hex_literal::hex!($x))
+        $crate::lifetimed_bytes::Bytes::from(&$crate::hex_literal::hex!($x) as &[u8])
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_macro() {
+        let a = bytes!["0080ff"];
+        let b = vec![0_u8, 128, 255];
+        assert_eq!(a, b);
+        assert_eq!(a, lifetimed_bytes::Bytes::from(b.as_slice()));
+    }
 }
